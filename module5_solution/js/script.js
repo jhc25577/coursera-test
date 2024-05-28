4fb99768ceacd120654f0f9af7132c7d3c578c2a
changj <chang07@ads.uni-passau.de>
2024-05-28 15:01:13 -0400

    fixing bug insertHtml cannot set properties of null

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -118,11 +118,11 @@ function buildAndShowHomeHTML (categories) {
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", String(chosenCategoryShortName));
 
-
+	  console.log(homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 	  
 
     },
